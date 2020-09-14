bbc76ef05f0fa7d67561688dec317d2ccb9a340f
Adrian Diaz <adrian93eh3@gmail.com>
2020-09-14 09:37:09 -0400

    resubmission fixes, still didn't get the quiz to end by reaching the end and couldn't get the score to stop counting down negatively upon answering too many wrong and reaching that

--- Assets/JS/highscore.js
+++ Assets/JS/highscore.js
@@ -1,14 +1,18 @@
-var highScoresEL = document.getElementById("highscores");
-var highScores = JSON.parse(localStorage.getItem("highscores"))
-    (highScores !== null){
-        highScores = highScores.sort(function(a, b){
-            return b.score - a.score;
-        });
-        console.log(highScores)
-
-        for (var i = 0; i < highScores.length; i++){
-            var li = document.createElement("li");
-            li.textContent = "initials: " + highScores[i].initials + "score: " + highScores[i].score
-            highScoresEL.appendChild(li)
-        }
+// CODE FROM MAX'S OFFICE HOURS FOR THIS ASSIGNMENT
+// ======================================================
+var highscoresEL = document.getElementById("highscore");
+var highScores = JSON.parse(localStorage.getItem("highscore"))
+if(highScores !== null){
+    console.log(highScores)
+    highScores = highScores.sort(function(a, b) {
+        return b.score - a.score;
+      });
+    
+    console.log(highScores)
+    
+    for (var i = 0; i < highScores.length; i++) {
+        var li = document.createElement("li");
+        li.textContent = "initials: " + highScores[i].initials + " score:  "+ highScores[i].score
+        highscoresEL.appendChild(li)
     }
+}
--- Assets/JS/index.js
+++ Assets/JS/index.js
@@ -1,140 +1,290 @@
-// Set all questions
-const questions = [
-  "What is my favorite color?",
-  "What's 2+6?",
-  "Which one is not an operator?",
-  "What languages are needed for a website?" 
-]
+// ALL THE DIFFERENT ATTEMPTS I HAVE TRIED
+// ================================================================================
 
-// Set all answers
-const allAnswers = [
-  {  
-    firstAnswer: "Blue",
-    secondAnswer: "Red",
-    thirdAnswer: "Green",
-    fourthAnswer: "Yellow"
-  },
-  {  
-    firstAnswer: "8",
-    secondAnswer: "4",
-    thirdAnswer: ".8",
-    fourthAnswer: "12"
-  },
-  {  
-    firstAnswer: "+",
-    secondAnswer: "*",
-    thirdAnswer: "|",
-    fourthAnswer: "/"
-  },
-  {
-    firstAnswer: "HTML",
-    secondAnswer: "CSS",
-    thirdAnswer: "JavaScript",
-    fourthAnswer: "All of them"
-  }
-]
-
-// Set all correct awnsers
-const correctAnswers = [
-  "Red",
-  "8",
-  "|",
-  "All of them"
-]
-
-// Set current score, question and answer
-let timePoints = 0;
-let correctAnswerNumber = 0;
-let updateOptionsCounter = 0;
-let updateQuestionsCounter = 0;
-let questionNumber = 1;
-let totalQuestions = questions.length;
+// var startButton = document.getElementById("startButton");
+// var mainContainerEl = document.querySelector(".container")
+// var timerEl = document.getElementById("timer");
+// var totalSeconds = 60;
+// var questionIndex = 0;
 
 
 
-// Get DOM elements
-const time = document.getElementById('time');
-const question = document.getElementById('question');
+// let question = [
+//     {
+//         questionTitle:
+//         answerChoices: 
+//         correctAnswer:
+//     },
+//     {
+//         questionTitle:
+//         answerChoices:
+//         correctAnswer: 0
+//     },
+//     {
+//         questionTitle:
+//         answerChoices:
+//         correctAnswer:
+//     },
+//     {
+//         questionTitle:
+//         answerChoices: 
+//         correctAnswer: 0
+//     },
+//     {
+//         questionTitle:
+//         answerChoices:
+//         correctAnswer: 2
+//     },
 
-// Update current question
-function nextQuestion() {
+// ];
 
-  // Increment 
-  correctAnswerNumber ++;
-  
-  // Update question number
-  (function updateQuestionNumber() {
-    questionNumber ++;
-    var questionNumberDom = document.getElementById('questionNumber');
-    questionNumberDom.innerHTML = questionNumber;
-    return questionNumber;
-  })();
+// startButton.addEventListener("click", startQuiz);
+// function startQuiz() {
 
-  
-  // update Options
-  (function updateOptions() {
-    updateOptionsCounter ++;
+//     startTimer()
+//     generateQuestion()
+// }
 
-    const answer1 = document.getElementById('answer1');
-    const answer2 = document.getElementById('answer2');
-    const answer3 = document.getElementById('answer3');
-    const answer4 = document.getElementById('answer4');
+// function generateQuestion() {
+//     mainContainerEl.innerHTML = "";
 
-    answer1.innerHTML = allAnswers[updateOptionsCounter].firstAnswer;
-    answer2.innerHTML = allAnswers[updateOptionsCounter].secondAnswer;
-    answer3.innerHTML = allAnswers[updateOptionsCounter].thirdAnswer;
-    answer4.innerHTML = allAnswers[updateOptionsCounter].fourthAnswer;
-  })();
+//     var questionEl = document.createElement("h3")
+//     questionEl.textContent = question[questionIndex].questionTitle
+//     mainContainerEl.appendChild(questionEl);
 
-  
-  // update question counter
-  (function updateQuestions() {
-    updateQuestionsCounter ++;
+//     for (var i = 0; i < question[questionIndex].answerChoices.length; i++) {
+//         var buttonEl = document.createElement("button")
+//         buttonEl.textContent = question[questionIndex].answerChoices[i]
+//         buttonEl.setAttribute("value", i)
+//         buttonEl.onclick = checkAnswer;
+//         mainContainerEl.appendChild(buttonEl)
+//     }
 
-    question.innerHTML = questions[updateQuestionsCounter];
-  })(); 
-  
-}
+// }
 
 
+// function checkAnswer() {
+//     if (this.value !== question[questionIndex].correctAnswer) {
+//         // make timer go down
+//     }
+//     questionIndex++;
+//     generateQuestion();
+// }
 // Check for the correct answer then show next question
-function checkAnswer(answeredQuestion) {
-  let answered = answeredQuestion.innerHTML; 
-  
-  if(answered == correctAnswers[correctAnswerNumber]) {
+// function checkAnswer(answeredQuestion) {
+//     let answered = answeredQuestion.innerHTML;
+
+//     if (answered == question[questionIndex].correctAnswer) {
+
+//         // Check for last question then end the quiz
+//         if (questionNumber == totalQuestions) {
+//             updateTime();
+//         }
+//         else {
+//             nextQuestion();
+//             updateTime();
+//         }
+//     }
+//     else {
+//         secondsLeft -= 15
+//         nextQuestion();
+//     }
+
+// =================================================================================
+// END OF PRIOR ATTEMPTS
+
+// GLOBAL VARIABLES
+// =======================
+var timerEl = document.getElementById("timer");
+var startButton = document.getElementById("start");
+var answersEl = document.getElementById("answers");
+var currentQuestion = 0;
+var secondsLeft = 60;
+var exam = document.getElementById('exam');
+var hsDiv = document.getElementById('hsDiv');
+var scoreEl = document.getElementById('score');
+var hsBtn = document.getElementById('hsBtn');
+var hsName = document.getElementById('hsName');
+
+// SETTING QUESTIONS HERE
+// ===========================
+var questions = [
+    {
+        title: "What does mixing the colors red and yellow give you?",
+        choices: [
+            "Yellowred",
+            "Redyellow",
+            "Orange",
+            "Green"
+        ],
+        answer: "Orange",
+    },
+    {
+        title: "Who is the main antagonist of most Mario games?",
+        choices: [
+            "Bowser",
+            "Wario",
+            "Waluigi",
+            "Mr. Game and Watch"
+        ],
+        answer: "Bowser",
+    },
+    {
+        title: "What does 12 to the power of 2 give you?",
+        choices: [
+            "12",
+            "0",
+            "24",
+            "144"
+        ],
+        answer: "144",
+    },
+    {
+        title: "Which boxer bit a chunk of another boxer's ear off during a fight?",
+        choices: [
+            "Muhammed Ali",
+            "Floyd Mayweather",
+            "Manny Pacquiao",
+            "Mike Tyson"
+        ],
+        answer: "Mike Tyson",
+    },
+    {
+        title: "What year did the internet become publicly available?",
+        choices: [
+            "1988",
+            "1984",
+            "1990",
+            "1993"
+        ],
+        answer: "1993",
+    },
+];
+
+// FUNCTION TO GENERATE QUESTIONS UPON QUIZ STARTING
+// ======================================================
+function generateQuestion() {
+    var questionEl = document.getElementById("questions")
+    questionEl.textContent = questions[currentQuestion].title;
+    answersEl.innerHTML = "";
+    for (var i = 0; i < questions[currentQuestion].choices.length; i++) {
+
+        // CREATING BTNS FOR QUESTION CHOICES
+        // ==============================================
+        var button = document.createElement("button");
+        button.textContent = questions[currentQuestion].choices[i];
+        // GIVES THE POSSIBLE CHOICES THEIR STYLING
+        // ==============================================
+        button.classList.add("btn", "btn-primary", "answer");
+        button.value = i;
+        // ATTACHING THE NEWLY CREATED BUTTONS TO THE ANSWERS DIV VIA THE "answersEl" VARIABLE
+        // ==============================================================================================
+        answersEl.appendChild(button);
+    }
+    }
+
+// FUNCTION TO START THE TIMER OF THE QUIZ
+// ==============================================
+function startTimer() {
+    setInterval(
+        () => {
+            secondsLeft--;
+            timerEl.textContent = "Time: " + secondsLeft + " seconds left"
+            if (secondsLeft <= 0) {
+                timerEl.textContent = "Time: 0 seconds left"
+                score();
+            }
+        },
+        1000
+    ) 
+}
+
+// FUNCTION TO HIDE THE WELCOME HTML
+// ======================================
+function init() {
+    // SETTING A VAR OF HOME TO AN ID OF HOME IN THE HTML
+    // ===========================================================
+    var home = document.getElementById("home");
+    // MAKING EVERYTHING WITHIN THAT DIV GO AWAY UPON QUIZ STARTING
+    // ==================================================================
+    home.setAttribute("class", "d-none");
+    generateQuestion();
+    startTimer();
+}
+
+// FUNCTION TO CHECK IF THE ANSWER IS CORRECT
+// ====================================================
+function checkAnswer() {
+    if (!e.target.classList.contains("answer")){
+        return
+    }
+    var btnValue = e.target.value
+    // DECREMENTING TIME/SCORE BY 15sec PER WRONG ANSWER
+    // ======================================================
+    if (questions[currentQuestion].choices[btnValue] !== questions[currentQuestion].answer) {
+        secondsLeft = secondsLeft - 15;
+    }
+    // GENERATE NEXT QUESTION UPON CHECKING THE ANSWER
+    // ====================================================
+    currentQuestion++;
+    generateQuestion();
+}
+
+
+// BEGINNING OF HIGHSCORE CODE
+// ===================================
+
+// EMPTY ARRAY TO DUMP JSON DATA OF INITIALS AND SCORE INTO
+// ==============================================================
+var highscore = [];
+
+
+function score() {
+    exam.classList.remove("d-block")
+    exam.classList.add("d-none");
+    hsDiv.classList.remove("d-none");
+    hsDiv.classList.add("d-block");
+    scoreEl.textContent = secondsLeft;
+}
+
+
+// EVENT LISTENER FOR SUBMIT BUTTON
+// ======================================
+hsBtn.addEventListener("click", function () {
+    if (localStorage.getItem("highscore")) {
+        // GRABS PREVIOUS HIGHSCORES FROM LOCAL STORAGE
+        // =============================================
+        highscore = JSON.parse(localStorage.getItem("highscore"));
+        // THE DATA THAT GETS PUSHED INTO THE EMPTY ARRAY OF "highscore" ABOVE
+        // =========================================================================
+        highscore.push({
+            initials: hsName.value,
+            score: secondsLeft
+        });
+        // SETS DATA OF JSON TO THE LOCAL STORAGE
+        // =============================================
+        localStorage.setItem("highscore", JSON.stringify(highscore));
+        // TAKES USER TO SPECIFIED HTML PAGE UPON SUBMITTING SCORE
+        // ==============================================================
+        window.location.href = "highscore.html";
+    } else {
         
-    // Check for last question then end the quiz
-    if (questionNumber == totalQuestions){
-      updateTime();
-    } 
-    else {
-      nextQuestion();  
-      updateTime();
+        highscore = [
+            {
+                initials: hsName.value,
+                score: secondsLeft
+            }
+        ];
+        // SETS DATA OF JSON TO THE LOCAL STORAGE
+        // =============================================
+        localStorage.setItem("highscore", JSON.stringify(highscore));
+        // TAKES USER TO SPECIFIED HTML PAGE UPON SUBMITTING SCORE
+        // ==============================================================
+        window.location.href = "highscore.html";
     }
-  } 
-  else {
-    secondsLeft -=15
-    nextQuestion();
-  }
-}
+});
 
-var timeEl = document.querySelector(".red");
-var secondsLeft = 45;
-function setTime() {
-  var timerInterval = setInterval(function() {
-    secondsLeft--;
-    timeEl.textContent = secondsLeft + "Time left";
-
-    if(secondsLeft <= 0) {
-      clearInterval(timerInterval);
-      alert("You are out of time");
-    }
-  }, 1000);
-}
-setTime();
-
-
-
-
-
-
+// ON CLICK EVENTS LISTENING FOR WHEN TO START QUIZ AND CHECK THE ANSWER THAT THE USER CHOSE
+// =================================================================================================
+startButton.addEventListener("click", init)
+answersEl.addEventListener("click", checkAnswer);
